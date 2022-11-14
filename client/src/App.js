8be6dc9cd212c55a81f35d5398c612fcd3fd97c0
Johnny Wong <johnny.wong2324@gmail.com>
2022-11-14 16:47:44 -0500

    setup socket listener for client

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,38 @@
-import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom'
+import React, { useEffect, useRef } from 'react';
+import io from 'socket.io-client';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { updateParkingSpots } from './redux/actions/parkingActions';
 
 import Routes from './routes';
 import Nav from './components/Nav';
 
 
 const App = () => {
+  const socketRef = useRef();
+  const dispatch = useDispatch();
+  const { parkingSpots } = useSelector(({ parking }) => parking)
+
+  // initialize client socket
+  useEffect(() => {
+    socketRef.current = io('/');
+    return () => {
+      socketRef.current.disconnect();
+    };
+  }, []);
+
+  // listen for 'parking-spots' to update current parking spots
+  useEffect(() => {
+    socketRef.current.on('parking-spots', ({ parkingSpots }) => {
+      console.log("Received New Parking Spots: ", parkingSpots);
+      dispatch(updateParkingSpots(parkingSpots));
+    })
+  }, [parkingSpots])
+
+  // https://github.com/AhmedBhati/video-streaming-with-flask-socketio
+  // Look at this to figure out how to stream video on client side with sockets
+  // Could be optional
+
   return (
     <Router>
       <div className='App'>
